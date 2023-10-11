f6673665daf7e728d69b1fb62c79d9b72d6bc65b
MandaBhaskaraKiran <141257355+MandaBhaskaraKiran@users.noreply.github.com>
2023-10-11 12:11:30 +0530

    happy first project

---
+++ SonorCloudProjectnew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SonorCloudProjectnew
+//
+//  Created by Manda U N V V Bhaskara Kiran on 11/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello")
+    }
+
+
+}
+
